f9b8c7b5f8f92a119cd3b2fb788b299135154bed
srikanthkanniyappan <srikanthkanniyappan.r@gmail.com>
2024-12-15 23:11:15 +0530

    Deploy On Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: process.env.PORT,
+  },
+  plugins: [vercel(), react()],
+  define: {
+    __APP_ENV__: process.env.VITE_API_URL,
+  },
+});
