b34c61b51cfe3e45a2d02cb8777eeda5d501a7d2
srikanthkanniyappan <srikanthkanniyappan.r@gmail.com>
2024-12-15 23:46:11 +0530

    Deploy On Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import flowbitePlugin from "flowbite/plugin";
+
 export default {
   content: [
     "./index.html",
@@ -8,5 +9,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [flowbitePlugin()],
 };
